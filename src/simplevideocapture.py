7d64cb0edf687502e3ce2e2934d8e8b4c44b4a0d
Alberto Soto <alsoto@hmc.edu>
2023-06-01 13:16:51 -0700

    Update simplevideocapture.py
    Updated citation and comments

--- src/simplevideocapture.py
+++ src/simplevideocapture.py
@@ -1,13 +1,12 @@
-# Python program to save a
-# video using OpenCV
+
+"""Program meant to save video captures from the overhead webcam above the fishtank. 
+Source code from: https://www.geeksforgeeks.org/saving-a-video-using-opencv/  
+Modified to change the filename and extension."""
 
 
 import cv2
 from datetime import *
 
-
-# Create an object to read
-# from camera
 video = cv2.VideoCapture(0)
 
 # We need to check if camera
@@ -15,54 +14,40 @@ video = cv2.VideoCapture(0)
 if (video.isOpened() == False):
 	print("Error reading video file")
 
-# We need to set resolutions.
-# so, convert them from float to integer.
+#getting resolutions 
 frame_width = int(video.get(3))
 frame_height = int(video.get(4))
 
+# generates date and time info for the video filename
 size = (frame_width, frame_height)
-
 now = str(datetime.now())
-
 filename = str(now[0:19])
 
-
-# Below VideoWriter object will create
-# a frame of above defined The output
-# is stored in 'filename.avi' file.
-result = cv2.VideoWriter(filename +".avi",
-						cv2.VideoWriter_fourcc(*'MJPG'),
-						30, size)
+# writes video to save -- filename, fourcc, framerate, size of video 
+result = cv2.VideoWriter(filename +".avi",cv2.VideoWriter_fourcc(*'MJPG'), 30, size)
 	
 while(True):
 	ret, frame = video.read()
 
 	if ret == True:
 
-		# Write the frame into the
-		# file 'filename.avi'
 		result.write(frame)
 
-		# Display the frame
-		# saved in the file
+		# Display the frame saved in the file
 		cv2.imshow('Frame', frame)
 
-		# Press S on keyboard
-		# to stop the process
-		if cv2.waitKey(1) & 0xFF == ord('s'):
+		# Press q on keyboard to end camera capture
+		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
-	# Break the loop
 	else:
 		break
 
-# When everything done, release
-# the video capture and video
-# write objects
+# When everything done, release the video capture and write objects
 video.release()
 result.release()
 	
-# Closes all the frames
+# closes windows
 cv2.destroyAllWindows()
 
-print("The video was successfully saved")
+print("The video was successfully saved as:", filename )
