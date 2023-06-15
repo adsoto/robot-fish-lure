21a83ba8c3769308598455061d653cc16563416e
Alberto Soto <alsoto@hmc.edu>
2023-06-15 08:43:05 -0700

    changed data plots to scatter plots and added straight line back and forth paths

--- data/plot_data.py
+++ data/plot_data.py
@@ -7,14 +7,18 @@ import matplotlib.pyplot as plt
 
 # referenced: https://www.tutorialspoint.com/how-to-plot-csv-data-using-matplotlib-and-pandas-in-python on 6/12/23
 #              https://www.statology.org/pandas-plot-multiple-series/ on 6/13/23
+#              https://stackoverflow.com/questions/44970881/matplotlib-multiple-scatter-subplots-with-shared-colour-bar
 
-df = pd.read_csv('data/06.13.2023/14.44.csv') # read csv file from data folder. Ex: 'data/06.13.2023/13.17.csv' reads the 13.17.csv file from the 06.13.2023 subfolder inside data folder
-print(df)
+df = pd.read_csv('data/06.14.2023/13.23.csv') # read csv file from data folder. Ex: 'data/06.13.2023/13.17.csv' reads the 13.17.csv file from the 06.13.2023 subfolder inside data folder
+print(df['Desired Position: y (m)'])
+for col in df.columns:
+    print(col)
+
 
 # Plot 1
 fig1, ax = plt.subplots()
-line1 = ax.plot(df['Desired Position: x (m)'], df['Desired Position: y (m)'])
-line2 = ax.plot(df['Actual Position: x (m)'],df['Actual Position: y (m)'])
+line1 = ax.scatter(df['Desired Position: x (m)'], df['Desired Position: y (m)'])
+line2 = ax.scatter(df['Actual Position: x (m)'],df['Actual Position: y (m)'])
 
 # set the legend for multiple plots
 ax.legend(['Desired Position', 'Actual Position'])
@@ -23,10 +27,13 @@ ax.legend(['Desired Position', 'Actual Position'])
 ax.set_title('Postions')
 ax.set_xlabel('X (m)')
 ax.set_ylabel('Y (m)')
+ax.set_ylim(0, 0.5)
+ax.set_xlim(0, 0.5)
+#ax.set_aspect(aspect=1)
 
 # Plot 2
 fig2, ax = plt.subplots()
-line1 = ax.plot(df['Theta vs. Time: time(s)'], df['Theta vs. Time: theta'])
+line1 = ax.scatter(df['Theta vs. Time: time(s)'], df['Theta vs. Time: theta'])
 #line2 = ax.plot(df['Actual Position: x (m)'],df['Actual Position: y (m)'])
 
 # set the legend for multiple plots
@@ -37,6 +44,44 @@ ax.set_title('Theta vs Time')
 ax.set_xlabel('Time (s)')
 ax.set_ylabel('Theta')
 
+
+# Plot 3
+fig3, ax = plt.subplots()
+line1 = ax.scatter(df['Robot Velocity: time (s)'], df['Robot Velocity: velocity (m/s)'])
+#line2 = ax.plot(df['Actual Position: x (m)'],df['Actual Position: y (m)'])
+
+# set the legend for multiple plots
+#ax.legend(['Desired Position', 'Actual Position'])
+
+# set title of plot
+ax.set_title('Velocity vs Time')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Velocity (m/s)')
+
+
+fig4, ax = plt.subplots()
+line1 = ax.scatter(df['Theta vs. Time: time(s)'], df['Desired Position: x (m)'])
+line2 = ax.scatter(df['Theta vs. Time: time(s)'], df['Actual Position: x (m)'])
+
+# set the legend for multiple plots
+#ax.legend(['Desired Position', 'Actual Position'])
+
+# set title of plot
+ax.set_title('X vs Time')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('X (m)')
+
+fig5, ax = plt.subplots()
+line1 = ax.scatter(df['Theta vs. Time: time(s)'], df['Desired Position: y (m)'])
+line2 = ax.scatter(df['Theta vs. Time: time(s)'], df['Actual Position: y (m)'])
+
+# set the legend for multiple plots
+#ax.legend(['Desired Position', 'Actual Position'])
+
+# set title of plot
+ax.set_title('Y vs Time')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Y (m)')
 
 
 ## tried using pandas dataframe to plot. could not figure out overlay with pandas
