9339eef4e4f58c98800a349c55e840eb0b1bd6e3
Alberto Soto <alsoto@hmc.edu>
2023-06-29 21:32:54 -0700

    updated way_point.py file with partially done turn function

--- src/waypoint_generator.py
+++ src/waypoint_generator.py
@@ -3,39 +3,56 @@ import numpy as np
 
 start_pos = np.array([0,0])
 start_time = 0
-dt = 0.05
+dt = 1
+start_theta = 0
 
-def straight_wypt(dist, vel, theta_dir, start_pos, start_time): # theta_dir is the direction that you want the line to be in 
-    """generates a single trajectory waypoint for the robot to move the desired distance at the desired speed """
-    new_x = round(start_pos[0]+dist*np.cos(theta_dir), 4)
-    new_y = round(start_pos[1]+dist*np.sin(theta_dir),4)
-    new_time = start_time+dist/vel
-    new_theta = round(theta_dir, 4) # idk if des_theta in the trajectory actually does anything
-    new_wypt = np.array([new_time, new_x, new_y, new_theta])
+# def straight_wypt(dist, vel, start_time, start_pos, theta_dir): # theta_dir is the direction that you want the line to be in 
+#     """generates a single trajectory waypoint for the robot to move the desired distance at the desired speed """
+#     new_x = round(start_pos[0]+dist*np.cos(theta_dir), 4)
+#     new_y = round(start_pos[1]+dist*np.sin(theta_dir),4)
+#     new_time = start_time+dist/vel
+#     new_theta = round(theta_dir, 4) # idk if des_theta in the trajectory actually does anything
+#     new_wypt = np.array([new_time, new_x, new_y, new_theta])
     
-    start_pos = [new_x, new_y]
-    start_time = new_time
-    # returns a waypoint and the start_pos and start_time required as input for a following waypoint
-    return np.array(new_wypt), start_pos, start_time
+#     start_pos = [new_x, new_y]
+#     start_time = new_time
+#     # returns a waypoint and the start_pos and start_time required as input for a following waypoint
+#     return np.array(new_wypt), start_time, start_pos, new_theta
 
-### straight waypoints
-wp1, start_pos, start_time = straight_wypt(1, 0.2, 0, start_pos, start_time)
-print(wp1)
-wp2 = straight_wypt(1, 0.2, 0, start_pos, start_time)[0]
-print(wp2)
+# ### straight
+# wp1, start_time1, start_pos1, start_theta1 = straight_wypt(1, 0.2, start_time, start_pos, start_theta)
+# print(wp1)
+# wp2 = straight_wypt(1, 0.2, start_time1, start_pos1, start_theta1)[0]
+# print(wp2)
 
-# def turn(radians, angular_vel, currrent_theta, start_pos, start_time):
-#     """turns the robot by _ radians at __ angular_velocity"""
+# waypoint format: [time, x, y, theta]
+def turn(radians, angular_vel, start_time, start_pos, current_theta):
+    """turns the robot by _ radians at __ angular_velocity"""
+    turn_t = radians/angular_vel
+    num_pts = round(turn_t/dt)
+    print(num_pts)
+    new_start_time = start_time+turn_t
+    full_rotations = int(radians/np.pi*2)
+    final_theta = "calc final theta"
+    wypts = np.array([])
+    for i in range(1, num_pts+1):
+        new_time = start_time+i*dt
+        ## if current_theta is + 
+        ## check when current_theta is negative
+        new_theta = current_theta + (radians)*(i/num_pts)
+        if new_theta > np.pi:
+            new_theta = -np.pi*2 + new_theta
+        elif new_theta < np.pi:
+            new_theta = new_theta +np.pi*2
 
-#     new_theta = current_theta
-#     new_wypt = np.array()
+        wypts = np.append(wypts, [new_time, start_pos[0], start_pos[1], new_theta])
 
-"""
-wypt1 = straight(1, 1, np.pi/2, start_pos, start_time)
-print(wypt1)
+    return wypts, new_start_time, start_pos, final_theta  
 
-def gen_wypts(wypt):
-"""
+wp3, start_time3, start_pos3, start_theta3 = turn(np.pi, np.pi/4, start_time, start_pos, start_theta)
+print(wp3)
+
+
 
 
 
