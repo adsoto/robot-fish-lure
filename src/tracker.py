f4b600eb640071c7ba679aaf077e909ae3f752e6
angelinaa-rodriguez <134556378+angelinaa-rodriguez@users.noreply.github.com>
2023-07-13 13:31:28 -0700

    Update tracker.py

--- src/tracker.py
+++ src/tracker.py
@@ -28,28 +28,28 @@ class EuclideanDistTracker:
                 if dist < 25:
                     self.center_points[id] = (cx, cy)
                     #print(self.center_points)
-                    objects_bbs_ids.append([x, y, w, h, id])
+                    objects_bbs_ids.append([id, x, y, w, h])
                     same_object_detected = True
                     break
 
             # New object is detected we assign the ID to that object
             if same_object_detected is False:
                 self.center_points[self.id_count] = (cx, cy)
-                objects_bbs_ids.append([x, y, w, h, self.id_count])
+                objects_bbs_ids.append([self.id_count, x, y, w, h])
                 self.id_count += 1
 
         # Clean the dictionary by center points to remove IDS not used anymore
         new_center_points = {}
         for obj_bb_id in objects_bbs_ids:
-            _, _, _, _, object_id = obj_bb_id
+            object_id, _, _, _, _ = obj_bb_id
             center = self.center_points[object_id]
             new_center_points[object_id] = center
 
         # Update dictionary with IDs not used removed
         self.center_points = new_center_points.copy()
-        sorted_pbjects_bbs_ids = sorted(objects_bbs_ids, key=lambda x: x[4]) 
+        sorted_pbjects_bbs_ids = sorted(objects_bbs_ids, key=lambda x: x[0]) 
         #print(sorted_pbjects_bbs_ids)
-        return objects_bbs_ids
+        return sorted_pbjects_bbs_ids
 
 
 
